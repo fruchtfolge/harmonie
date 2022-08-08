2a316d2583539c00349838e331f031b17c32e191
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2022-08-08 12:01:04 +0200

    ➖ remove elan-parser dependency -> moved into this repo

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,16 +1,16 @@
 import { XMLParser } from 'fast-xml-parser'
 import { read } from 'shapefile'
-import { join, parseXML, parseGML } from 'elan-parser'
+import { join, parseXML, parseGML } from './parseDataExperts.js'
 
-export default {
-  async shape (shp, dbf) {
-    return read(shp, dbf)
-  },
-  xml (xml) {
-    const parser = new XMLParser({ ignoreAttributes: false }, true)
-    return parser.parse(xml, true)
-  },
-  dataExperts (xml, gml) {
-    return join(parseXML(xml), parseGML(gml))
-  }
+export function shape (shp, dbf) {
+  return read(shp, dbf)
+}
+
+export function xml (xml) {
+  const parser = new XMLParser({ ignoreAttributes: false }, true)
+  return parser.parse(xml, true)
+}
+
+export function dataExperts (xml, gml) {
+  return join(parseXML(xml), parseGML(gml))
 }
